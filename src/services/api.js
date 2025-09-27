46d0edbeba4ef5cc43e2d504496656655cc2f255
JoshDominguezV <jdomiinguezz@gmail.com>
2025-09-26 22:11:46 -0600

    localhost

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { normalizeData } from './normalizeService';
 
-const API_BASE_URL = 'http://192.168.2.219:4000';
+const API_BASE_URL = 'http://localhost:4000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
